dd494835b6b81d8136cff886f390c8f98e2e36c6
pipinstalltrh <trhatwork@gmail.com>
2026-05-02 21:04:18 +0000

    Chat feature ready

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+import sys
+
 def main():
-    print("Hello from my-agent!")
+    
+    load_dotenv()
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.getenv("API_KEY"),
+    )
+    if len(sys.argv)<2:
+        print("I need a prompt !!")
+        sys.exit(1)
+        
+    prompt = sys.argv[1]
+    
+    
+    
+    
+    response = client.chat.completions.create(
+        model="z-ai/glm-4.5-air:free",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        
 
+    )
 
-if __name__ == "__main__":
-    main()
+    print(response.choices[0].message.content)
+
+    print("Token usage: ")
+    print(f"Sent tokens (prompt): {response.usage.prompt_tokens}")
+    print(f"Received tokens (completion): {response.usage.completion_tokens}")
+    
+main()
