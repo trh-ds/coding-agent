2afbed5bfa1de8dd45801b2594a0fcafd2aa28a1
pipinstalltrh <trhatwork@gmail.com>
2026-05-02 21:27:47 +0000

    Tools : get_fiole_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,8 +7,17 @@ def get_files_info(working_directory, directory="."):
         print(f"Error: The directory '{directory}' is not within the working directory '{working_directory}'.")
         return
     
+    final_response =""
     
+    contents = os.listdir(abs_directory)
+    for content in contents:
+        content_path = os.path.join(abs_directory, content)
+        if os.path.isfile(content_path):
+            size = os.path.getsize(content_path)
+            final_response += f"File: {content}, Size: {size} bytes\n"
+        elif os.path.isdir(content_path):
+            final_response += f"Directory: {content}\n"
 
-    
-    
-get_files_info(os.getcwd(), ".")
+    return final_response
+
+get_files_info(os.getcwd(), "/mnt/c/Users/Tirth Patel/OneDrive/Desktop/Python_Projects/AI_Agent/calculator")
